33106ab75cae48f9428b4392aea4a60ee1d19f9b
Vx-tech <xaviervianney8@gmail.com>
2026-04-08 23:03:19 +0300

    Connecting endpoints

--- dist/utils/generateLoanReference.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateLoanReference = void 0;
-const generateLoanReference = () => {
-    const timestamp = Date.now();
-    const randomPart = Math.floor(1000 + Math.random() * 9000);
-    return `LN-${timestamp}-${randomPart}`;
-};
-exports.generateLoanReference = generateLoanReference;
